ca85b0d0cca2bd6d3e5b6a305d6fb38b02da7cd0
Ouditchya Sinha <osinha6792@gmail.com>
2013-07-06 14:05:55 +0530

    High School Maths.

---
+++ AP2.cpp
@@ -0,0 +1,38 @@
+// AC, ALGO : High School Maths.
+
+/* 
+
+ Helpful Link : http://library.thinkquest.org/20991/gather/formula/data/240.html
+
+*/
+
+// For any clarifications, contact me at : osinha6792@gmail.com
+
+#include<cstdio>
+
+using namespace std ;
+
+int main( )
+{
+  int t ;
+  long long x , y , z , a , n , d , i ;
+	
+  scanf("%d",&t) ;
+	
+  while( t-- )
+	{
+		scanf("%lld%lld%lld",&x,&y,&z) ;
+		
+    n = ( 2 * z ) / ( x + y ) ;
+		d = ( y - x ) / ( n - 5 ) ;
+		a = x - 2 * d ;
+		
+    printf("%lld\n",n) ;
+		
+    for( i = 0 ; i < n ; i++ )
+			printf("%lld ",a+i*d) ;
+	  printf("\n") ;
+	}
+	
+  return 0 ;
+}
