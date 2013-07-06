7bbd2a71bed29e55e91889011fd77f53635def24
Ouditchya Sinha <osinha6792@gmail.com>
2013-07-06 15:31:30 +0530

    Maths, Co-ordinate Geometry.

--- SICRANO.cpp
+++ SICRANO.cpp
@@ -15,7 +15,9 @@ using namespace std ;
 int main()
 {
         int t , n , m ;
+        
         scanf("%d",&t) ;
+        
         while( t-- )
         {
                 scanf("%d%d",&n,&m) ;
@@ -31,17 +33,21 @@ int main()
                 for( i = 1 ; i <= m ; i++ )
                 {
                         int x1 , x2 , y1 , y2 , x , X , y , Y , y3 , x3 , m1 , m2 ;
+                        
                         x1 = s[i][0] ; 
                         x2 = s[i][2] ;
                         y1 = s[i][1] ;
                         y2 = s[i][3] ;
+                        
                         x = ( x1 > x2 ) ? x2 : x1 ;
                         X = ( x1 > x2 ) ? x1 : x2 ;
                         y = ( y1 > y2 ) ? y2 : y1 ;
                         Y = ( y1 > y2 ) ? y1 : y2 ;
+                        
                         m1 = y2 - y1 ;
                         m2 = x2 - x1 ;
                         ctr = 0 ;
+                        
                         for( j = 1 ; j <= n ; j++ )
                         {
                                 x3 = p[j][0] ;
@@ -83,8 +89,10 @@ int main()
                                         }
                                 }
                         }
+                        
                         printf("%d\n",ctr) ;
                 }
         }
+        
         return 0 ;
 }
