7cd05b72bc14b4d455c3ed77661a6c2cd9a5dd9a
Ouditchya Sinha <osinha6792@gmail.com>
2013-07-13 18:49:13 +0530

    Adhoc.

---
+++ JULKA.py
@@ -0,0 +1,13 @@
+# AC, ALGO : Adhoc.
+
+# For any clarifications, contact me at : osinha6792@gmail.com
+
+t = 10
+while t :
+ t -= 1
+ x = int( raw_input( ) )
+ y = int( raw_input( ) )
+ z = ( x + y ) / 2
+ print z
+ z = x - z
+ print z 
