49047fa9d141591d27ffd08be30e1fd18fcc0948
Ouditchya Sinha <osinha6792@gmail.com>
2013-07-13 18:46:44 +0530

    Greedy, Activity Selection problem.

---
+++ BUSYMAN.cpp
@@ -0,0 +1,55 @@
+// AC, ALGO : Greedy, Activity Selection problem.
+
+/* Some Helpful Links :
+
+  http://www.cplusplus.com/reference/utility/pair/
+  http://en.wikipedia.org/wiki/Activity_selection_problem
+  http://www.geeksforgeeks.org/greedy-algorithms-set-1-activity-selection-problem/
+
+*/
+
+// For any clarifications, contact me at : osinha6792@gmail.com
+
+#include<cstdio>
+#include<utility>
+#include<algorithm>
+
+using namespace std ;
+
+int main( )
+{
+        int t , i , s , f , n , ctr , k ;
+        
+        scanf("%d",&t) ;
+        
+        pair<int,int> a[100001] ;
+        
+        while( t-- )
+        {
+                scanf("%d",&n) ;
+                
+                for( i = 0 ; i < n ; i++ )
+                {
+                        scanf("%d%d",&s,&f) ;
+                        a[i] = make_pair(f,s) ;
+                }
+                
+                sort( a , a + n ) ;
+                
+                ctr = 1 ;
+                k = 0 ;
+                
+                for( i = 1 ; i <= n ; i++ )
+                {
+                        if( a[i].second >= a[k].first )
+                        {
+                                ctr++ ;
+                                k = i ;
+                        }
+                }
+                
+                printf("%d\n",ctr) ;
+        }
+        
+        return 0 ;
+}
