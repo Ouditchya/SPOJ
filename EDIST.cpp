ccd02b5a1e9ccec3a1f1efac8e7185423429d1c6
Ouditchya Sinha <osinha6792@gmail.com>
2013-07-14 16:10:10 +0530

    Dynamic Programming, Edit distance.

---
+++ EDIST.cpp
@@ -0,0 +1,54 @@
+// AC , ALGO : Dynamic Programming, Edit distance.
+
+/* Some Helpful Links :
+
+http://en.wikipedia.org/wiki/Dynamic_programming
+http://people.csail.mit.edu/bdean/6.046/dp/
+http://www.codechef.com/wiki/tutorial-dynamic-programming
+http://www.cs.miami.edu/~burt/learning/Csc517.101/workbook/recurrence_dynprog.html
+
+*/
+
+// For any clarifications, contact me at : osinha6792@gmail.com
+
+#include<cstdio>
+#include<cstring>
+
+using namespace std ;
+
+inline int min( int a , int b , int c )
+{
+  return ( c > ( ( a > b ) ? b : a ) ) ? ( ( a > b ) ? b : a ) : c ;
+}
+
+int a[2101][2101] ;
+
+int main( )
+{
+	int t , i , j , n , m ;
+	char x[2101] , y[2101] ;
+	
+	for( scanf("%d",&t) ; t ; t-- )
+	{
+		scanf("%s %s",x,y) ;
+		
+		n = strlen( x ) ;
+		m = strlen( y ) ;
+		
+		a[0][0] = 0 ;
+		for( i = 1 ; i <= n ; i++ )
+			a[i][0] = a[i-1][0] + 1 ;
+		for( j = 1 ; j <= m ; j++ )
+			a[0][j] = a[0][j-1] + 1 ;
+			
+		for( i = 1 ; i <= n ; i++ )
+		{
+			for( j = 1 ; j <= m ; j++ )
+				a[i][j] = min( a[i-1][j] + 1 , a[i][j-1] + 1 , a[i-1][j-1] + ( ( x[i-1] == y[j-1] ) ? 0 : 1 ) ) ;
+		}
+		
+		printf("%d\n",a[n][m]) ;
+	}
+	
+	return 0 ;
+}
