e45bbc4a30101cd84c4bc3db8a5a1ba68a568553
Ouditchya Sinha <osinha6792@gmail.com>
2013-07-19 00:00:04 +0530

    Maths, GCD.

---
+++ GCD2.py
@@ -0,0 +1,19 @@
+# AC , ALGO : Maths, GCD.
+
+# Helpful Link : https://en.wikipedia.org/wiki/Greatest_common_divisor
+
+# For any clarifications, contact me at : osinha6792@gmail.com
+
+def gcd( a , b ):
+    if b == 0:
+        return a
+    else:
+        return gcd( b , a % b )
+ 
+t = int(input())
+while t != 0:
+    s = input()
+    a , b = (int(x) for x in s.split())
+    c = gcd( a , b )
+    print(c)
+    t = t - 1
