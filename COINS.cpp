86c76fd8de46f467b76ed15cb2d7e36809c2fdd9
Ouditchya Sinha <osinha6792@gmail.com>
2013-07-19 23:02:12 +0530

    Dynamic Programming : Simple Recurrence

---
+++ COINS.cpp
@@ -0,0 +1,48 @@
+// AC, ALGO : Simple DP
+
+/* Some Helpful Links :
+
+  http://en.wikipedia.org/wiki/Dynamic_programming
+  http://people.csail.mit.edu/bdean/6.046/dp/
+  http://www.codechef.com/wiki/tutorial-dynamic-programming
+  http://www.cs.miami.edu/~burt/learning/Csc517.101/workbook/recurrence_dynprog.html
+  http://www.cplusplus.com/reference/map/map/
+
+*/
+
+// For any clarifications, contact me at : osinha6792@gmail.com
+
+#include<cstdio>
+#include<map>
+
+using namespace std ;
+
+typedef long long int_64 ;
+
+map<int_64, int_64> h ;
+
+inline int_64 dp( int_64 n )
+{
+    if( n == 0 )
+        return 0 ;
+        
+    int_64 r = h[n] ;
+    
+    if( r == 0 )
+    {
+        r = max( n , dp( n / 2 ) + dp( n / 3 ) + dp( n / 4 ) ) ;
+        h[n] = r ;
+    }
+    
+    return r ;   
+}
+
+int main( )
+{
+    int_64 n ;
+    
+    while( ~scanf("%lld",&n) )
+        printf("%lld\n",dp(n)) ;
+
+    return 0 ;
+}
